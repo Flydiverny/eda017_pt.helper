1d3e75008abde957dc0d939e5deb2ff290d8973a
Markus Maga <markus@nadilus.com>
2013-02-08 15:01:05 +0100

    moving to own package

--- inlmn5/src/se/markusmaga/lth/pt/helper/menu/IMenuAction.java
+++
@@ -1,6 +0,0 @@
-package se.markusmaga.lth.pt.helper.menu;
-
-public interface IMenuAction {
-	public void execute();
-	public String toString();
-}
--- inlmn5/src/se/markusmaga/lth/pt/helper/menu/IMenuHandler.java
+++
@@ -1,6 +0,0 @@
-package se.markusmaga.lth.pt.helper.menu;
-
-public interface IMenuHandler {
-	public void add(IMenuAction ma);
-	public void showMenuAndExecuteAction();
-}
