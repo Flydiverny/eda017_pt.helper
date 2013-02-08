5763aa6740180fe235db1df1b5127d9372d9c6e1
Markus Maga <markus@nadilus.com>
2013-02-08 21:00:26 +0100

    Generates initial pattern
    Signed-off-by: Markus Maga <markus@nadilus.com>

--- inlmn5/src/se/markusmaga/lth/pt/mandelbrot/Launcher.java
+++ inlmn5/src/se/markusmaga/lth/pt/mandelbrot/Launcher.java
@@ -9,6 +9,7 @@ public class Launcher extends OptionsLauncher {
 	private static final int WINDOW_WIDTH = 500;
 	private static final int WINDOW_HEIGHT = 500;
 	private static final int PIXEL_SIZE = 1;
+	private static final int MAX_ITERATIONS = 255;
 	
 	public static void main(String[] args) {
 		new Launcher().run();
@@ -18,7 +19,13 @@ public class Launcher extends OptionsLauncher {
 		int width = getIntValue("width");
 		int height = getIntValue("height");
 		int pixels = getIntValue("pixels");
+		int iterations = getIntValue("iterations");
 		DotWindow w = new DotWindow(width, height, 1);
+		w.useRectangularDots();
+		w.setAutoUpdate(false);
+		
+		Mandelbrot m = new Mandelbrot(iterations);
+		m.display(w);
 	}
 
 	/**
@@ -38,5 +45,6 @@ public class Launcher extends OptionsLauncher {
 		addOption("width", "Ange fönster bredd", WINDOW_WIDTH);
 		addOption("height", "Ange fönster höjd", WINDOW_HEIGHT);
 		addOption("pixels", "Ange pixel storlek", PIXEL_SIZE);
+		addOption("iterations", "Ange pixel storlek", MAX_ITERATIONS);
 	}
 }
--- inlmn5/src/se/markusmaga/lth/pt/mandelbrot/Mandelbrot.java
+++ inlmn5/src/se/markusmaga/lth/pt/mandelbrot/Mandelbrot.java
@@ -1,18 +1,28 @@
 package se.markusmaga.lth.pt.mandelbrot;
 
 import se.lth.cs.pt.dots.listeners.DotWindow;
+import se.lth.cs.pt.dots.Color;
+import java.awt.Point;
 
 public class Mandelbrot {
-	public static final int MAX_ITERATIONS = 100;
+	public static final int MAX_ITERATIONS	= 255;
+	public static final double START_SCALE	= 2;
+	public static final double START_RE		= -0.5;
+	public static final double START_IM		= 0.0;
 	
 	private int max_iterations;
+	private double currentScale;
+	private double targetRe, targetIm;
 	
-	public Mandelbrot() {
-		this(MAX_ITERATIONS);
+	public Mandelbrot()
+		this(MAX_ITERATIONS, START_RE, START_IM, SCALE);
 	}
 	
-	public Mandelbrot(int max_iterations) {
-		this.max_iterations=max_iterations;
+	public Mandelbrot(int max_iterations, double re, double im, double scale) {
+		this.max_iterations	= max_iterations;
+		this.targetRe		= re;
+		this.targetIm		= im;
+		this.currentScale	= scale;
 	}
 	
 	private int calculate(double re, double im) {
@@ -34,9 +44,43 @@ public class Mandelbrot {
 		return iterations;
 	}
 	
+	public void setScale(double scale) {
+		this.currentScale = scale;
+	}
+	
+	public void setCenter(double re, double im) {
+		this.targetRe = re;
+		this.targetIm = im;
+	}
+	
+	private double xToRe(double x, double width) {
+		return coordToComplex(x, width, targetRe);
+	}
+	
+	private double yToIm(double y, double height) {
+		return coordToComplex(y, height, targetIm);
+	}
+	
+	private double coordToComplex(double c, double dim, double tar) {
+		return (currentScale/2-tar)*-1+currentScale/dim*c;
+	}
+	
 	public void display(DotWindow w) {
 		for(int x = 0; x < w.getWidth(); x++) {
-			
+			for(int y = 0; y < w.getHeight(); y++) {
+				double im = yToIm(y, w.getHeight());
+				double re = xToRe(x, w.getWidth());
+				
+				int iterations = calculate(re, im);
+				w.setDot(x, y, new Color(iterations, 0, 0));
+				
+				//System.out.println("(" + x + ", "+ y +") - (" + re + ", " + im + ") Iterations: " + iterations);
+				if(x == w.getWidth()-1 && y == w.getHeight()-1 || x==0 && y==0) {
+					System.out.println("Im: " + im + " Re: " + re + " y: " + y + " x: " + x + " Height: " + w.getHeight() + " Width: " + w.getWidth() + " Iterations: " + iterations);
+				}
+			}
 		}
+		
+		w.update();
 	}
 }
